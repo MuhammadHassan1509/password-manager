36c0358ec164269cd9029847fb769525b35f2115
Muhammad Hassan <muhammadhassan10c@gmail.com>
2025-09-01 19:31:13 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         <button className='text-white bg-green-700 rounded-lg flex gap-2 items-center' onClick={() => window.open("https://github.com/MuhammadHassan1509/password-manager", "_blank")}
         >
 
-          <img className='invert w-8' src={Github} alt="" /><span className='text-center pr-2'>GitHub</span>
+          <img className='invert w-8' src={githubLogo} alt="" /><span className='text-center pr-2'>GitHub</span>
         </button>
       </div>
     </nav>
